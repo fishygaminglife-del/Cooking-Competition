c726a1120f7c6ae24ae13d15048834dde952d176
fishygaminglife-del <fishygaming.life@gmail.com>
2026-04-19 08:15:40 -0500

    Adding collision bodys.
    Making game functional.

---
+++ cooking-competition/scripts/homescreen.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var x = 4
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_chopping_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_mixing_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_pizza_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_coffe_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_packing_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
---
+++ cooking-competition/scripts/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+const SPEED = 150
+
+func _physics_process(delta):
+	topdown_movement(delta)
+	move_and_slide()
+
+func topdown_movement(delta):
+	var directionx = Input.get_axis("left", "right")
+	var directiony = Input.get_axis("up", "down")
+
+	velocity.x = directionx * SPEED
+	velocity.y = directiony * SPEED
+
+	#if directionx == 0 and directiony == 0:
+		#if last_facing == 0:
+			#$MCCharater.play("downidle")
+		#elif last_facing < 0:
+			#$MCCharater.play("sideidle2")
+		#elif last_facing > 0:
+			#$MCCharater.play("sideidle")
+			
+	#else:
+		#if directiony < 0:
+			#$MCCharater.play("up")
+			#last_facing = 0
+		#elif directiony > 0:
+			#last_facing = 0
+			#$MCCharater.play("down")
+		#elif directionx < 0:
+			#last_facing = 1
+			#$MCCharater.play("side")
+		#elif directionx > 0:
+			#last_facing = -1
+			#$MCCharater.play("side2")
