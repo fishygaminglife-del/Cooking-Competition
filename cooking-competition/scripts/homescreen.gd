18e72c601d2339fe33a0954b2f0afe6171d3f9f0
fishygaminglife-del <fishygaming.life@gmail.com>
2026-04-21 20:47:37 -0500

    Fixed area enter bugs.

--- cooking-competition/scripts/homescreen.gd
+++ cooking-competition/scripts/homescreen.gd
@@ -1,11 +1,5 @@
 extends Node2D
 
-var in_chopping = false
-var in_mixing = false
-var in_pizza = false
-var in_coffee = false
-var in_packing = false
-var in_bake = false
 var area = 0
 func _ready() -> void:
 	$CuttingUI.visible = false
@@ -20,21 +14,7 @@ func _ready() -> void:
 	$Cheeseonpizza.visible = false
 func _process(delta: float) -> void:
 	pass
-func update_area():
-	if in_chopping:
-		area = 1
-	elif in_mixing:
-		area = 2
-	elif in_pizza:
-		area = 3
-	elif in_coffee:
-		area = 4
-	elif in_packing:
-		area = 5
-	elif in_bake:
-		area = 6
-	else:
-		area = 0
+
 func _input(event):
 	if event.is_action_pressed("interact"):
 		if area == 1:
@@ -60,21 +40,21 @@ func _input(event):
 		elif area == 5:
 			if not $Cookedpizza.visible:
 				if not $Pizza.visible:
-					$Banner/pizzaprint.text = "need to have pizza"
+					$Banner/pizza.text = "Get Pizza"
 				else:
-					$Banner/pizzaprint.text = "need to cook pizza"
+					$Banner/pizza.text = "Cook Pizza"
 			if not $Smoothie.visible:
 				if not $FruitSmoothie.visible:
-					$Banner/smoothieprint.text = "chop vegetables"
+					$Banner/smoothie.text = "Chop Vegetables"
 				else:
-					$Banner/smoothieprint.text = "mix vegetables"
+					$Banner/smoothie.text = "Mix Vegetables"
 			if not $Coffe.visible:
-				$Banner/coffeprint.text = "brew coffe"
+				$Banner/coffe.text = "Brew Coffe"
 			
 			if 	$Cookedpizza.visible and $Coffe.visible and $Smoothie.visible:
 				$Banner/pizza.text = ""
 				$Banner/coffe.text = ""
-				$Banner/smoothieprint.text = "completed"
+				$Banner/smoothieprint.text = "Completed"
 				$PackedOrder.visible = true
 			$AnimationPlayer.play("banner_move")
 		elif area == 6:
@@ -91,8 +71,7 @@ func _on_chopping_body_entered(body: Node2D) -> void:
 	$pizza/pizzae.visible = false
 	$coffe/coffee.visible = false
 	$packing/packe.visible = false
-	in_chopping = true
-	update_area()
+	area = 1
 
 func _on_mixing_body_entered(body: Node2D) -> void:
 	$chopping/Chope.visible = false
@@ -100,8 +79,7 @@ func _on_mixing_body_entered(body: Node2D) -> void:
 	$pizza/pizzae.visible = false
 	$coffe/coffee.visible = false
 	$packing/packe.visible = false
-	in_mixing = true
-	update_area()
+	area = 2
 
 func _on_pizza_body_entered(body: Node2D) -> void:
 	$chopping/Chope.visible = false
@@ -109,8 +87,7 @@ func _on_pizza_body_entered(body: Node2D) -> void:
 	$pizza/pizzae.visible = true
 	$coffe/coffee.visible = false
 	$packing/packe.visible = false
-	in_pizza = true
-	update_area()
+	area = 3
 
 func _on_coffe_body_entered(body: Node2D) -> void:
 	$chopping/Chope.visible = false
@@ -118,41 +95,34 @@ func _on_coffe_body_entered(body: Node2D) -> void:
 	$pizza/pizzae.visible = false
 	$coffe/coffee.visible = true
 	$packing/packe.visible = false
-	in_coffee = true
-	update_area()
+	area = 4
 func _on_packing_body_entered(body: Node2D) -> void:
 	$chopping/Chope.visible = false
 	$mixing/Mixe.visible = false
 	$pizza/pizzae.visible = false
 	$coffe/coffee.visible = false
 	$packing/packe.visible = true
-	in_packing = true
-	update_area()
+	area = 5
 func _on_chopping_body_exited(body: Node2D) -> void:
 	$chopping/Chope.visible = false
 	$Node2D.SPEED = 150
-	in_chopping = false
-	update_area()
+	area = 0
 func _on_mixing_body_exited(body: Node2D) -> void:
 	$mixing/Mixe.visible = false
 	$Node2D.SPEED = 150
-	in_mixing = false
-	update_area()
+	area = 0
 func _on_pizza_body_exited(body: Node2D) -> void:
 	$pizza/pizzae.visible = false
 	$Node2D.SPEED = 150
-	in_pizza = false
-	update_area()
+	area = 0
 func _on_coffe_body_exited(body: Node2D) -> void:
 	$coffe/coffee.visible = false
 	$Node2D.SPEED = 150
-	in_coffee = false
-	update_area()
+	area = 0
 func _on_packing_body_exited(body: Node2D) -> void:
 	$packing/packe.visible = false
 	$Node2D.SPEED = 150
-	in_packing = false
-	update_area()
+	area = 0
 func _on_bake_body_entered(body: Node2D) -> void:
 	$chopping/Chope.visible = false
 	$mixing/Mixe.visible = false
@@ -160,9 +130,7 @@ func _on_bake_body_entered(body: Node2D) -> void:
 	$coffe/coffee.visible = false
 	$packing/packe.visible = false
 	$bake/bakee.visible = true
-	in_bake = true
-	update_area()
+	area = 0
 func _on_bake_body_exited(body: Node2D) -> void:
 	$bake/bakee.visible = false
-	in_bake = false
-	update_area()
+	area = 0
